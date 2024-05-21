f2da1b3bf8e904fbcddecac0e96904c476ef87fa
DuzeW <wiktor.wojcicki05@gmail.com>
2024-05-21 23:59:02 +0200

    new gui working

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	gui "github.com/grupawp/warships-gui/v2"
 )
 
@@ -21,6 +22,27 @@ func main() {
 
 	ctx := context.Background()
 	ui.Start(ctx, nil)
+
+	txt := gui.NewText(1, 1, "Press Ctrl+C to exit", nil)
+	ui.Draw(txt)
+	ui.Start(context.TODO(), nil)
+
+	txt = gui.NewText(1, 1, "Press on any coordinate to log it.", nil)
+	ui.Draw(txt)
+	ui.Draw(gui.NewText(1, 2, "Press Ctrl+C to exit", nil))
+
+	board = gui.NewBoard(1, 4, nil)
+	ui.Draw(board)
+
+	go func() {
+		for {
+			char := board.Listen(context.TODO())
+			txt.SetText(fmt.Sprintf("Coordinate: %s", char))
+			ui.Log("Coordinate: %s", char) // logs are displayed after the game exits
+		}
+	}()
+
+	ui.Start(context.TODO(), nil)
 	//	for {
 	//		playerInfo()
 	//		setShips()
