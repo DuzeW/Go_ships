3e2e76ac11b85f950956b7bd3ccaeacb8c5fb285
Wiktor <wiktor_laptop@example.com>
2024-06-03 23:27:10 +0200

    i know how to make game loop

--- dumpster/dumpsterGame.go
+++ dumpster/dumpsterGame.go
@@ -1,1 +1,53 @@
 package dumpster
+
+controllerHTTP.GameStatus()
+
+controllerHTTP.GameStatus()
+char := opBoard.Listen(context.TODO())
+result := controllerHTTP.Fire(char)
+if result == "miss" {
+miss = append(miss, char)
+}
+if result == "hit" || result == "sunk" {
+hit = append(hit, char)
+}
+if result == "sunk" {
+for i := 0; i < len(hit); i++ {
+x, y := coordToInts(hit[i])
+for j := -1; j < 2; j++ {
+for k := -1; k < 2; k++ {
+if x+j <= 9 && x+j >= 0 && y+k <= 9 && y+k >= 0 {
+miss = append(miss, intsToCoord(x+j, y+k))
+}
+}
+}
+}
+}
+for i := 0; i < len(miss); i++ {
+x, y := coordToInts(miss[i])
+opStates[x][y] = gui.Miss
+}
+for i := 0; i < len(hit); i++ {
+x, y := coordToInts(hit[i])
+opStates[x][y] = gui.Hit
+}
+txt.SetText(fmt.Sprintf("Twój ruch Coordinate: %s %s Pozostały czas %d Skuteczność strzałów: dobra", char, result, controllerHTTP.Timer))
+opBoard.SetStates(opStates)
+ui.Log("Coordinate: %s", char)
+for i := 0; i < len(controllerHTTP.OppShots); i++ {
+x, y := coordToInts(controllerHTTP.OppShots[i])
+states[x][y] = gui.Miss
+}
+for i := 0; i < len(controllerHTTP.OppShots); i++ {
+x, y := coordToInts(controllerHTTP.OppShots[i])
+for j := 0; j < len(myCoords); j++ {
+if controllerHTTP.OppShots[i] == myCoords[j] {
+states[x][y] = gui.Hit
+}
+}
+}
+board.SetStates(states)
+controllerHTTP.GameStatus()
+if controllerHTTP.Status != "game_in_progress" {
+break
+}
--- game/game.go
+++ game/game.go
@@ -5,16 +5,19 @@ import (
 	"fmt"
 	gui "github.com/grupawp/warships-gui/v2"
 	"ships/controllerHTTP"
+	"time"
 )
 
 func ShowBoards() {
 	ui := gui.NewGUI(true)
 
 	txt := gui.NewText(1, 1, "Press on any coordinate to log it.", nil)
+	takenCoords := gui.NewText(1, 3, "Press on any coordinate to log it.", nil)
 	ui.Draw(txt)
+	ui.Draw(takenCoords)
 	ui.Draw(gui.NewText(1, 2, "Naciśnij Ctrl+C żeby się poddać", nil))
 
-	board := gui.NewBoard(1, 4, nil)
+	board := gui.NewBoard(1, 5, nil)
 	myCoords := controllerHTTP.GetMyBoard()
 	//myCoords := []string{"A1", "A2", "A3", "A4", "A6", "A7", "A8", "C6", "C7", "C8", "D1", "D2", "F1", "F2", "H1", "H2", "H10", "J5", "J7", "J9"}
 
@@ -27,67 +30,33 @@ func ShowBoards() {
 	ui.Draw(board)
 
 	opStates := [10][10]gui.State{}
-	opBoard := gui.NewBoard(50, 4, nil)
+	opBoard := gui.NewBoard(50, 5, nil)
 	opBoard.SetStates(opStates)
 	ui.Draw(opBoard)
 
-	var miss []string
-	var hit []string
+	//var miss []string
+	//var hit []string
 	go func() {
-		controllerHTTP.GameStatus()
+		i := 60
 		for {
-
-			controllerHTTP.GameStatus()
-			char := opBoard.Listen(context.TODO())
-			result := controllerHTTP.Fire(char)
-			if result == "miss" {
-				miss = append(miss, char)
-			}
-			if result == "hit" || result == "sunk" {
-				hit = append(hit, char)
-			}
-			if result == "sunk" {
-				for i := 0; i < len(hit); i++ {
-					x, y := coordToInts(hit[i])
-					for j := -1; j < 2; j++ {
-						for k := -1; k < 2; k++ {
-							if x+j <= 9 && x+j >= 0 && y+k <= 9 && y+k >= 0 {
-								miss = append(miss, intsToCoord(x+j, y+k))
-							}
-						}
-					}
-				}
-			}
-			for i := 0; i < len(miss); i++ {
-				x, y := coordToInts(miss[i])
-				opStates[x][y] = gui.Miss
-			}
-			for i := 0; i < len(hit); i++ {
-				x, y := coordToInts(hit[i])
-				opStates[x][y] = gui.Hit
-			}
-			txt.SetText(fmt.Sprintf("Twój ruch Coordinate: %s %s Pozostały czas %d Skuteczność strzałów: dobra", char, result, controllerHTTP.Timer))
-			opBoard.SetStates(opStates)
-			ui.Log("Coordinate: %s", char)
-			for i := 0; i < len(controllerHTTP.OppShots); i++ {
-				x, y := coordToInts(controllerHTTP.OppShots[i])
-				states[x][y] = gui.Miss
-			}
-			for i := 0; i < len(controllerHTTP.OppShots); i++ {
-				x, y := coordToInts(controllerHTTP.OppShots[i])
-				for j := 0; j < len(myCoords); j++ {
-					if controllerHTTP.OppShots[i] == myCoords[j] {
-						states[x][y] = gui.Hit
-					}
-				}
-			}
-			board.SetStates(states)
-			controllerHTTP.GameStatus()
-			if controllerHTTP.Status != "game_in_progress" {
-				break
-			}
+			i--
+			txt.SetText(fmt.Sprintf("Czas na ruch %d", i))
+			time.Sleep(1 * time.Second)
 		}
 	}()
+	go func() {
+		for {
+			char := opBoard.Listen(context.TODO())
+			takenCoords.SetText(fmt.Sprintf("Czas na ruch %s", char))
+		}
+	}()
+	go func() {
+		for {
+			controllerHTTP.GameStatus()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
 	ui.Start(context.TODO(), nil)
 }
 func coordToInts(coord string) (int, int) {
